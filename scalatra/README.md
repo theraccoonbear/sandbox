8eda857e7f3b2da83bc41fabefcd9af627b10194
Don Smith <smith@earthlinginteractive.com>
2015-09-16 17:33:39 -0500

    work with Scalatra

---
+++ scalatra/README.md
@@ -0,0 +1,17 @@
+## Notes on Scalatra
+
+* Followed instructions found here: http://scalatra.org/getting-started/installation.html
+* Struggled a bit finding v7 (1.7.x) of JVM, located here: http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html
+* I haven't really touched Java or the JVM apart from playing Minecraft in years.  Getting up and running fully with it and Scala/tra took a little time, but wasn't particularly painful.
+* Automatic code reloading on change is useful (http://scalatra.org/getting-started/first-project.html#toc_102):
+* Unable to find current Scala syntax highlighting support for my editor (Komodo Edit) but did at least get Jade template support: https://github.com/rkgarcia/JadeLang4Komodo/blob/master/jade_language-0.2-ko.xpi
+* Views can use SSP, SCAML, Mustache, or Jade with the sample project using Jade. http://scalatra.org/2.4/guides/views/scalate.html#toc_314
+* Took a little hunting to find info on serving static files: http://scalatra.org/getting-started/project-structure.html#toc_86
+* NOTE: Scalate (Scala Template Engine) does not implement Jade template parsing as node.js does.  Multiple name="value" attribues on an element should be space, not comma, separated.  This tripped me up for a while: http://stackoverflow.com/questions/15146471/jade-scalate-template-error-invalidsyntaxexception
+* Waiting for recompile of templates/code on save is a bit annoying, but not unbearable.
+* Added:
+    "org.scalaquery" % "scalaquery_2.9.0-1" % "0.9.5",
+    "postgresql" % "postgresql" % "9.1-901.jdbc4"
+  to ./project/build.scala's libraryDependencies    
+
+        
---
+++ scalatra/scalatratest/project/build.scala
@@ -0,0 +1,49 @@
+import sbt._
+import Keys._
+import org.scalatra.sbt._
+import org.scalatra.sbt.PluginKeys._
+import com.mojolly.scalate.ScalatePlugin._
+import ScalateKeys._
+
+object ScalatratestBuild extends Build {
+  val Organization = "com.earthlinginteractive"
+  val Name = "ScalatraTest"
+  val Version = "0.1.0-SNAPSHOT"
+  val ScalaVersion = "2.11.6"
+  val ScalatraVersion = "2.4.0-RC2-2"
+
+  lazy val project = Project (
+    "scalatratest",
+    file("."),
+    settings = ScalatraPlugin.scalatraSettings ++ scalateSettings ++ Seq(
+      organization := Organization,
+      name := Name,
+      version := Version,
+      scalaVersion := ScalaVersion,
+      resolvers += Classpaths.typesafeReleases,
+      resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
+      libraryDependencies ++= Seq(
+        "org.scalatra" %% "scalatra" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.2.10.v20150310" % "container",
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+        "org.scalaquery" % "scalaquery_2.9.0-1" % "0.9.5",
+        "postgresql" % "postgresql" % "9.1-901.jdbc4"
+      ),
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
+        Seq(
+          TemplateConfig(
+            base / "webapp" / "WEB-INF" / "templates",
+            Seq.empty,  /* default imports should be added here */
+            Seq(
+              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
+            ),  /* add extra bindings here */
+            Some("templates")
+          )
+        )
+      }
+    )
+  )
+}
