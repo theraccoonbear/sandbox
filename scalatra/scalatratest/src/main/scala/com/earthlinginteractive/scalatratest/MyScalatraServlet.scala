5b76d3eecd0f3e034aa56ed6300379339194c688
Don Smith <don.c.smith@gmail.com>
2015-09-18 13:05:10 -0500

    more pg fiddlin

--- scalatra/scalatratest/src/main/scala/com/earthlinginteractive/scalatratest/MyScalatraServlet.scala
+++ scalatra/scalatratest/src/main/scala/com/earthlinginteractive/scalatratest/MyScalatraServlet.scala
@@ -2,15 +2,39 @@ package com.earthlinginteractive.scalatratest
 
 import org.scalatra._
 import scalate.ScalateSupport
-import java.sql.{Connection, DriverManager, ResultSet};
+import java.sql.DriverManager
+import java.sql.Connection
 
 
 class MyScalatraServlet extends ScalatratestStack {
   before() {
     val dbc = "jdbc:postgresql://localhost:5432/scalatra_app?user=scalatrauser&password=password"
-    classOf[com.postgresql.jdbc.Driver]
-    val conn = DriverManager.getConnection(dbc)
-    val statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE)
+    // connect to the database named "mysql" on the localhost
+    val driver = "com.postgresql.jdbc.Driver"
+    // val url = "jdbc:postgresql://localhost/scalatra_app"
+    // val username = "scalatrauser"
+    // val password = "password"
+ 
+    // there's probably a better way to do this
+    var connection:Connection = null
+ 
+    try {
+      // make the connection
+      Class.forName(driver)
+      //connection = DriverManager.getConnection(url, username, password)
+ 
+      // create the statement, and run the select query
+      //val statement = connection.createStatement()
+      //val resultSet = statement.executeQuery("SELECT * FROM user")
+      //while ( resultSet.next() ) {
+      //  val host = resultSet.getString("username")
+      //  val pass = resultSet.getString("password")
+      //  println("host, pass = " + host + ", " + pass)
+      //}
+    } catch {
+      case e => e.printStackTrace
+    }
+    connection.close()
   }
   
   get("/") {
