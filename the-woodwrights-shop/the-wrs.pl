c12c2671d30f1ebfb88a51dd6cc6bf40d6e6e9fc
Don Smith <don.c.smith@gmail.com>
2016-03-14 08:17:50 -0500

    quieter wget

--- the-woodwrights-shop/the-wrs.pl
+++ the-woodwrights-shop/the-wrs.pl
@@ -14,7 +14,14 @@ my $chunks = $wrs->getEpisodeChunkList($episodes->[0]->{id});
 
 my $c_idx = 0;
 foreach my $c (@$chunks) {
-	print STDERR "Fetching $c\n";
-	my $cmd = "wget -O ./data/tmp/chunk_" . sprintf('%04d', ++$c_idx) . ".ts '$c'";
-	system($cmd);
+	#print STDERR "Fetching $c\n";
+	my $chunk_path = "./data/tmp/chunk_" . sprintf('%04d', ++$c_idx) . ".ts";
+	if (-f $chunk_path) {
+		print STDERR '.';
+	} else {
+		print STDERR "  Grabbing chunk $c_idx of " . scalar @$chunks . "\n";
+		my $cmd = "wget -nv -O $chunk_path '$c' 2>&1 > /dev/null";
+		system($cmd);
+		print STDERR "Done.";
+	}
 }
