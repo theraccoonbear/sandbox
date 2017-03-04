dfc9a34808db5e536d010ce0701e1a555bf3ce45
Don Smith <don.c.smith@gmail.com>
2017-03-04 13:16:56 -0600

    debug

--- iron-chef/fetcher.pl
+++ iron-chef/fetcher.pl
@@ -81,7 +81,7 @@ foreach my $season (@$results) {
 			my $episode_path = sprintf('%s/Season %d/*s%de%02d', $iron_chef_root, $s_num, $s_num, $episode->{number});
 			$episode_path =~ s/\s/\\ /g;
 			$episode->{path} = $episode_path;
-			my $files = [glob $episode_path];
+			my $files = [grep { -e $_ } glob $episode_path];
 			$episode->{glob_match} = $files;
 			push @$new_episodes, $episode;
 		}
