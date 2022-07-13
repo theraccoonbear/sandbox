c666ccb698f0d76bfc7cff3d4df75d4493c52555
Don Smith <dosmith@rei.com>
2022-07-13 15:29:57 -0500

    master: basic install, password extract, and browser launch

--- jenkins-setup/local.sh
+++ jenkins-setup/local.sh
@@ -8,3 +8,5 @@ echo Installing Jenkins as $ME@$HOST
 scp on-host.sh $ME@$HOST:/home/$ME/on-host.sh
 
 ssh -t $ME@$HOST "chmod +x on-host.sh && ./on-host.sh"
+
+open http://$HOST:8080/login
--- jenkins-setup/on-host.sh
+++ jenkins-setup/on-host.sh
@@ -14,3 +14,25 @@ sudo yum upgrade
 sudo yum install java-11-openjdk
 sudo yum install jenkins
 sudo systemctl daemon-reload
+
+# start the Jenkins service
+sudo systemctl start jenkins
+
+JENKINS_PASS=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+
+echo -e "\n\n\n\n"
+echo "ATTENTION!!!! You will need this password in the next step!\n"
+echo "    JENKINS PASSWORD: $JENKINS_PASS"
+echo -e "\n"
+sleep 1 && echo -n "."
+sleep 1 && echo -n "."
+sleep 1 && echo -n "."
+sleep 1 && echo -n "."
+
+echo -e "\n\nPress any key to acknowledge you've copied the password..."
+while [ true ] ; do
+    read -t 3 -n 1
+    if [ $? = 0 ] ; then
+        exit;
+    fi
+done
